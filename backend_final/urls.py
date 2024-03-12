1272e5e1baa37999a406a8cc21564d53265de83d
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-03-12 18:57:20 +0000

    add create group and list

--- backend_final/urls.py
+++ backend_final/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from myapp.views import AppointmentListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('appointments/', AppointmentListView.as_view(), name='appointment-list'),
+    path('api-auth/', include('rest_framework.urls'))
+
+    # path('doctors/', include('doctors.urls')),#
 
 ]
