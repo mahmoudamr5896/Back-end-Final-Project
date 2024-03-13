122ad758ad90844386ff93fab49c231daa860450
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-03-13 00:31:22 +0000

    #

--- backend_final/urls.py
+++ backend_final/urls.py
@@ -29,8 +29,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     #path('', include(router.urls)),
     path('reviews-all/', include(router.urls)),
-
-
     path('api-auth/', include('rest_framework.urls')) 
 
 ]
